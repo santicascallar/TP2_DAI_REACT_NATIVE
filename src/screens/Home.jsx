14b62eb56b149c3b635f099853804b4852ac8a07
Home <santiagoarielcascallar@gmail.com>
2022-10-19 02:32:44 -0300

    contactos

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,26 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Flatlist } from 'react-native';
-import * as a contacts from 'expo-contacts';
+import * as Contacts from 'expo-contacts';
 import {useState, useEffect} from 'react';
 
 export default function App() {
+  let [error, setError] = useState(undefined);
+  const [contactos, setContactos] = useState(undefined);
 
-  const [contacto, setContactos] = useState([]);
-  //https://www.youtube.com/watch?v=PII2YRRhtI8 para el contacts
   useEffect(()=>{
-    (async()=>{
-      const {status}=await Contacts.requestPermissionsAsync();
-      if(status === 'granted'){
-        const {data}=await Contacts.getContactsAsync({
-          <fieldset disabled="disabled"></fieldset>
-        })
+    (async () => {
+      const {status}= await Contacts.requestPermissionsAsync();
+
+      if(status === 'granted') {
+        const {data}= await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.FirstName, Contacts.Fields.LastName, Contacts.Fields.PhoneNumbers]
+        });
+
+        if(data.length > 0){
+          console.log(data);
+          setContactos(data);
+        }
+        else{
+          setError("No se han encontrado contactos")
+        }
       }
-    })()
-  })
+      else {
+        setError("Permiso denegado");
+      }
+    })
+  ()
+  }, [])
+
+  let getNumero = (contact) => {
+    if (contact.phoneNumbers) {
+      return contact.phoneNumbers.map((phoneNumber, index) => {
+        <View key={index}>
+          <Text>Telefono: {phoneNumber.number}</Text>
+        </View>
+      })
+    }
+  }
+
+  let getContactos = () => {
+    if(contactos !== undefined){
+      return contactos.map((contact, index) => {
+        return (
+          <View key={index}>
+            <Text>Nombre: {contact.firstName} {contact.lastName}</Text>
+            {getNumero(contact)}
+          </View>
+        );
+      });
+    }
+    else{
+      return <Text>Esperando contactos...</Text>
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <Text>navigation!</Text>
+      <Text>Lista Contactos</Text>
+      {getContactos()}
       <StatusBar style="auto" />
     </View>
   );
