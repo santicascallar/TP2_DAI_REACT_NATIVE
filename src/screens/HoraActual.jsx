0572acf3a8146c8a0535794fe93dfecf53ad8a02
santicascallar <101258956+santicascallar@users.noreply.github.com>
2022-10-23 23:32:20 -0300

    avance

--- src/screens/HoraActual.jsx
+++ src/screens/HoraActual.jsx
@@ -5,15 +5,16 @@ import * as Location from 'expo-location';
 export default function HoraActual() {
   let [error, setError] = useState(undefined);
   const [location, setLocation] = useState(undefined);
+  const [currentHour, setCurrentHour] = useState('');
   
    useEffect(() => {
     (async () => {
       
-      const { status } = await Location.requestForegroundPermissionsAsync();
+      const { status } = await Permissions.askAsync(Permissions.LOCATION);
       
       if (status !== 'granted') {
-        setError('Permission to access location was denied');
-        return;
+        console.log('error no permisos')
+        setError('El permiso a la Localizacion ha sido denegado');
       }
 
       let localizar = await Location.getCurrentPositionAsync({});
@@ -21,10 +22,24 @@ export default function HoraActual() {
       
     })();
   }, []);
-
+  
+  useEffect(() => {
+    let hours = new Date().getHours()
+    let min = new Date().getMinutes()
+    let sec = new Date().getSeconds()
+    
+    setCurrentHour(hours + ':' + min + ':' + sec)
+  }, []);
+  
+  //Obtener el clima: https://www.youtube.com/watch?v=M8mslcfiEQI
+  //https://developer.weatherunlocked.com/signup/success (Confirmar Email)
+  //https://www.youtube.com/watch?v=MjJ1A-Puszw (Obtener current Hour)
+  
   return (
     <View style={styles.container}>
       <Text>Hora y Clima</Text>
+      <Text>Ubicacion: {localizar}</Text>
+      <Text>Hora: {CurrentHour}</Text>
     </View>
   );
 }
