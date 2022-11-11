d30eb0171fa4aae491433b7fa28755118fe89dfe
santicascallar <101258956+santicascallar@users.noreply.github.com>
2022-11-11 09:44:51 -0300

    Update HoraActual.jsx

--- src/screens/HoraActual.jsx
+++ src/screens/HoraActual.jsx
@@ -19,7 +19,8 @@ export default function HoraActual() {
         return;
       }
 
-      let localizar = await Location.getCurrentPositionAsync({});
+      let localizar = await Location.getCurrentPositionAsync({})
+      //.then()
       setLocation(localizar);
     })();
   }, []);
